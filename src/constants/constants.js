6953fa4da2b5af1b00f6cbf6e223c5505aac4641
Ohad <Ohad.Mor.Yosef@gmail.com>
2021-12-23 17:32:52 +0200

    Timeline data edited

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -42,9 +42,15 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { startYear: 2013, endYear: 2016, description: "Military service IDF" },
+  {
+    startYear: 2017,
+    endYear: 2020,
+    description: "Assistant Manager - Bug Multisystem Ltd.",
+  },
+  {
+    startYear: 2020,
+    endYear: 2021,
+    description: "Fullstack Web Developer Studies at John Bryce",
+  },
 ];
