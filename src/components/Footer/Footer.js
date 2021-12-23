5e7f56055f4bfb5b0038c79c033f92e9e8cebbaa
Ohad <Ohad.Mor.Yosef@gmail.com>
2021-12-23 23:10:51 +0200

    Completed 2 sections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  SocialContainer,
+  SocialIconsContainer,
+  Support,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+972-503-882-169">+972-503-882-169</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>E-Mail</LinkTitle>
+          <LinkItem href="mailto:Ohad.Mor.Yosef@gmail.com">
+            Ohad.Mor.Yosef@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Support>
+            For all enquiries, please contant me using the info above or the
+            social media
+          </Support>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons
+            href="https://www.linkedin.com/in/ohadmy/"
+            target="_blank"
+          >
+            <AiFillLinkedin size="3rem"></AiFillLinkedin>
+          </SocialIcons>
+          <SocialIcons href="https://github.com/OhadMY" target="_blank">
+            <AiFillGithub size="3rem"></AiFillGithub>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact } from "react-icons/di";
+import { RiServerLine } from "react-icons/ri";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle main>Technologies</SectionTitle>
+    <SectionText>
+      Here are some of the technologies I have been working with so far
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.Js <br />
+            Angular
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <RiServerLine size="3rem" style />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.Js <br />
+            Express
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Database</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            MYSQL <br />
+            MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
